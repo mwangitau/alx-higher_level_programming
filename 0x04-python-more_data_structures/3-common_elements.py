67819e8d6f833b59d617312e06df547eed5083ed
Jaw Knee <mwangijohngitau1@gmail.com>
2022-09-12 12:55:11 +0300

    Function that returns a set of common elements in two sets

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    """Return a set of all elements present in only one set."""
+    """Return a set of common elements present in two sets."""
     return (set_1 ^ set_2)
