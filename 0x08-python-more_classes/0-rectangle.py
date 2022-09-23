3f18f97ccb428ad70967a1e20269b598550f15b3
Jaw Knee <mwangijohngitau1@gmail.com>
2022-09-23 16:09:42 +0300

    Empty class Rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""
-Contains a definition for an empty Class Rectangle.
-"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """Empty class"""
+    """Represent a rectangle."""
     pass
