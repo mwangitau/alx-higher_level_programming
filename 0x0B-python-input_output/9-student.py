ddfc7188cac762c950a01bd3b354ba85cfd5a164
Jaw Knee <mwangijohngitau1@gmail.com>
2022-10-05 12:52:44 +0300

    Class student that defines a student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""contains the class "Student"
 """
+Contains the clas "Student"
+"""
+
 
 class Student:
     """Representation of a student"""
     def __init__(self, first_name, last_name, age):
-        """initializes the student"""
+        """Initializes the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-     def to_json(self):
-            """returns a dictionary representation of a student instance"""
-            return self.__dict__
-
+    def to_json(self):
+        """returns a dictionary representation of a Student instance"""
+        return self.__dict__
