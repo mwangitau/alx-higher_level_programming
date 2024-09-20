d2c5d0d236f5bda147df2f76807a445572c1e98d
mwangitau <mwangijohngitau1@gmail.com>
2024-09-20 20:05:08 +0300

    some change

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -45,21 +45,3 @@ def print_solution(board):
 def nqueens(n):
     board = [[0 for _ in range(n)] for _ in range(n)]
     solve_nqueens(board, 0, n)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    nqueens(n)
