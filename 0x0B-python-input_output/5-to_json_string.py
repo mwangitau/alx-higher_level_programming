29530ae1b3dac6d0073041e2a8c90fb0c460a177
mwangitau <mwangijohngitau1@gmail.com>
2024-09-20 21:27:24 +0300

    io

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-# 5-to_json_string.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines a string-to-JSON function."""
-import json
+from json import dumps
 
 
 def to_json_string(my_obj):
-    """Return the JSON representation of a string object."""
-    return json.dumps(my_obj)
+    return dumps(my_obj)
--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-# 7-save_to_json_file.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines a JSON file-writing function."""
-import json
+from json import dumps
 
 
 def save_to_json_file(my_obj, filename):
-    """Write an object to a text file using JSON representation."""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
+    with open(filename, mode='w', encoding='utf-8') as f:
+        f.write(dumps(my_obj))
